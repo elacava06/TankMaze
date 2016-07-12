c8ed3941ba690a79038092f2caa78ef327b29ae8
Eric LaCava <elacava@mit.edu>
2016-07-12 16:49:13 -0400

    Tank body can now pick up collectibles

--- Assets/Scripts/tankBody.cs
+++ Assets/Scripts/tankBody.cs
@@ -9,8 +9,9 @@ public class tankBody : MonoBehaviour
 
     //private variables
     private Rigidbody2D body;
-    
-    
+    private GameObject collectibleHolding;
+
+
     // Called once at start:
     void Start()
     {
@@ -26,7 +27,7 @@ public class tankBody : MonoBehaviour
 
 
 
-// Helper functions:
+    // Helper functions:
 
 
     // Changes heading angle based on L/R arrow key press/hold
@@ -44,5 +45,23 @@ public class tankBody : MonoBehaviour
         body.velocity = new Vector3(0, 0, 0);
         Vector3 heading = transform.rotation * Vector3.up;
         body.velocity = heading * movementSpeed * movementChange;
+    }
+
+
+    // On collision with the collectible, picks it up if not holding one
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "collectible")
+        {
+            if (collectibleHolding == null)
+            {
+                // Sets the collectible on the hood of the tank
+                collectibleHolding = collision.gameObject;
+                collectibleHolding.transform.SetParent(transform);
+                collectibleHolding.GetComponent<SpriteRenderer>().sortingOrder = 1;
+                collectibleHolding.transform.localPosition = new Vector3(0, 1.43f, 0);
+            }
+            
+        }
     }
 }
