7e3afffcfd931495e26fb8214f529ad8fa5e9b6a
Isaac <isaac.rosado95@gmail.com>
2016-07-12 14:32:47 -0400

    Added rotation to turret

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Represents the turret on top of the tank
+ */
+public class Turret : MonoBehaviour {
+
+    public float turnSpeed;
+
+	// Use this for initialization
+	void Start () {
+        turnSpeed = 60;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        turnTurret();
+	}
+
+    void turnTurret()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("turret"), 0);
+        this.transform.Rotate(Vector3.forward, direction * turnSpeed);
+    }
+}
