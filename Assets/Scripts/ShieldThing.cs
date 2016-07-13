ae3a621d6ef0bc64cb8d8c848956a8e9f0c8ed48
emoryram <emoryram@gmail.com>
2016-07-13 12:36:55 -0400

    shield sprite and script added

---
+++ Assets/Scripts/ShieldThing.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldThing : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    private int rotationSpeed = 0;
+
+    // Update is called once per frame
+    void Update() {
+        incrementRotation();
+    }
+
+    private void incrementRotation()
+    {
+        float change = Input.GetAxis("shield");
+        transform.Rotate(Vector3.forward, -change * rotationSpeed);
+        while (System.Math.Abs(rotationSpeed) < 10)
+        {
+            if (Input.GetKey("v"))
+            {
+                rotationSpeed += 1;
+            }
+            if (Input.GetKey("n"))
+            {
+                rotationSpeed -= 1;
+            }
+        }
+    }
+
+}
