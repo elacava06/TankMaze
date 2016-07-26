f500f84b31fbca0f67df90c2ff781f44d1f67fb6
vancisesam <vancisesam@gmail.com>
2016-07-26 13:26:56 -0400

    map select and stuffs

---
+++ Assets/Scripts/MapsSelector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MapsSelector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void loadMap(int mapNumber)
+    {
+        SceneManager.LoadScene(mapNumber);
+    }
+}
